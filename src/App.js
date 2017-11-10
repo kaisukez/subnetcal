cd036c85bbd4d716e7ec0911d694eefe2e99caf5
kaisukez <zykigix@hotmail.com>
2017-11-10 17:19:10 +0700

    add ip class

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ip from 'ip';
 import './App.css';
 
+import Header from './components/header'
+import Footer from './components/footer'
 import InputBlock from './components/input-block'
 import Result from './components/result'
 
@@ -9,20 +11,23 @@ class App extends Component {
   constructor(props) {
     super(props)
     let baseIP = "158.108.0.1";
-    let baseSubnet = "255.255.255.0";
+    let baseSubnetValue = "255.255.255.0";
+    let baseIPClass = "C";
 
     this.state = {
       hasResult: false,
 
       networkClass: "any",
-      subnetValue: baseSubnet,
+      subnetValue: baseSubnetValue,
       subnetNumber: 24,
       ip: baseIP,
       options: this.generateSubnet(32),
 
+      ipClass: baseIPClass,
+
       commitIP: baseIP,
-      commitSubnetValue: baseSubnet,
-      commitNetworkClass: "any",
+      commitSubnetValue: baseSubnetValue,
+      commitIPClass: baseIPClass,
     }
   }
 
@@ -31,7 +36,10 @@ class App extends Component {
   }
 
   subnetHandler(event) {
-    this.setState({subnetValue: event.target.value})
+    this.setState({
+      subnetValue: event.target.value,
+      ipClass: this.calIPClass(event.target.value)
+    })
   }
 
   ipHandler(event) {
@@ -40,10 +48,10 @@ class App extends Component {
 
   commit() {
     this.setState({
+      hasResult: true,
       commitNetworkClass: this.state.networkClass,
       commitSubnetValue: this.state.subnetValue,
-      commitIP: this.state.ip,
-      hasResult: true
+      commitIPClass: this.state.ipClass,
     })
   }
 
@@ -55,8 +63,8 @@ class App extends Component {
     return Array(quantity).fill().map((_, i) => {
       return {
         name: ip.fromPrefixLen(32-i) + ' / ' + (32-i),
-        number: 32-i,
-        value: ip.fromPrefixLen(32-i)
+        value: 32-i,
+        number: ip.fromPrefixLen(32-i)
       }
     })
   }
@@ -78,13 +86,21 @@ class App extends Component {
     this.setState({ options })
   }
 
-  calIPClass() {
-
+  calIPClass(subnetValue) {
+    if(subnetValue >= 24)
+      return "C";
+    else if(subnetValue >= 16)
+      return "B";
+    else if(subnetValue >= 8)
+      return "A";
+    else
+      return "None";
   }
 
   render() {
     return (
       <div className="App">
+        <Header />
         <InputBlock
           networkClassHandler={this.networkClassHandler.bind(this)}
           subnetHandler={this.subnetHandler.bind(this)}
@@ -92,18 +108,19 @@ class App extends Component {
           commit={this.commit.bind(this)}
           clear={this.clear.bind(this)}
           options={this.state.options}
-          subnetValue={this.state.subnetValue}
+          subnetNumber={this.state.subnetValue}
           ip={this.state.ip}
         />
         <Result
           hasResult={this.state.hasResult}
           networkClass={this.state.networkClass}
-          subnetValue={this.state.subnetValue}
+          subnetNumber={ip.fromPrefixLen(this.state.subnetValue)}
           ip={this.state.ip}
           commitIP={this.state.commitIP}
-          commitSubnetValue={this.state.commitSubnetValue}
-          commitNetworkClass={this.state.commitNetworkClass}
+          commitSubnetNumber={ip.fromPrefixLen(this.state.commitSubnetValue)}
+          commitIPClass={this.state.commitIPClass}
         />
+        <Footer />
       </div>
     );
   }
